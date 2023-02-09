09b7128bb969d57a2af9dbc5c4893f4b299696c3
Pierre Braem <pierrebraem@orange.fr>
2023-02-09 14:20:54 +0100

    FEAT: Page d'accueil pour les non-connectés

---
+++ insamedia/resources/views/accueilN.blade.php
@@ -0,0 +1,24 @@
+@extends('layout.app')
+
+@section('content')
+    <div id="container">
+        <h1 class="titre">Bienvenue sur Insamedia</h1>
+        <div class="card formulaire centrer">
+            <div class="card-body">
+                <form>
+                    <div class="form-group">
+                        <label>Adresse mail :</label>
+                        <input type="email" class="form-control" name="email" />
+                    </div>
+                    <div class="form-group">
+                        <label>Mot de passe :</label>
+                        <input type="password" class="form-control" name="mdp" />
+                    </div>
+                    <button type="submit" class="btn btn-primary centrer mt-3">Connexion</button>
+                </form>
+                <p class="petiteLigne">Pas de compte? Vous pouvez en créer un un <a href="#">ici</a>
+                <p class="petiteLigne">Mot de passe oublié? <a href="#">Réinitialisez-le</a>
+            </div>
+        </div>
+    </div>
+@endsection
--- insamedia/resources/views/template/enteteUtilisateurN.blade.php
+++ insamedia/resources/views/template/enteteUtilisateurN.blade.php
@@ -1,6 +1,8 @@
 <head>
     <meta charset="UTF-8">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <link href="{{ asset('css/style.css') }}" rel="stylesheet" />
+
     <title>Insamedia Accueil</title>
 <head>
 <body>
