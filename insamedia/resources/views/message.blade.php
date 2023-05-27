23ee09653f66fa584c7fcab6fcedbf9718c70ef1
Pierre Braem <pierrebraem@orange.fr>
2023-05-27 17:38:45 +0200

    Zone discussion

--- insamedia/resources/views/message.blade.php
+++ insamedia/resources/views/message.blade.php
@@ -1,183 +1,269 @@
 @extends('layout.app')
 
 @section('content')
-    <div id="listcontact">
-        <div class="card">
-            <div class="card-body">
-                <div class="row">
-                    <div class="w-25">
+    <div id="containerM">
+        <div id="listcontact">
+            <div class="card">
+                <div class="card-body">
+                    <div class="row">
+                        <div class="w-25">
+                            <div class="col">
+                                <img class="photoProfile elementDroite" src="{{ asset('images/photo_default.jpg') }}" alt="default"/>
+                            </div>
+                        </div>
                         <div class="col">
-                            <img class="photoProfile elementDroite" src="{{ asset('images/photo_default.jpg') }}" alt="default"/>
+                            <div class="row">
+                                <p>Cumulo</p>
+                            </div>
+                            <div class="row">
+                                <p class="apercuMessage">Vous : Salut, comment tu vas? - 18H</p>
+                            </div>
                         </div>
                     </div>
-                    <div class="col">
-                        <div class="row">
-                            <p>Cumulo</p>
+                </div>
+            </div>
+
+            <div class="card">
+                <div class="card-body">
+                    <div class="row">
+                        <div class="w-25">
+                            <div class="col">
+                                <img class="photoProfile elementDroite" src="{{ asset('images/photo_exemple1.png') }}" alt="default"/>
+                            </div>
                         </div>
-                        <div class="row">
-                            <p class="apercuMessage">Vous : Salut, comment tu vas? - 18H</p>
+                        <div class="col">
+                            <div class="row">
+                                <p>FrenchBrave</p>
+                            </div>
+                            <div class="row">
+                                <p class="apercuMessage">Cool, heureux d'apprendre que tu vas bien - 2H</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="card">
+                <div class="card-body">
+                    <div class="row">
+                        <div class="w-25">
+                            <div class="col">
+                                <img class="photoProfile elementDroite" src="{{ asset('images/photo_exemple2.png') }}" alt="default"/>
+                            </div>
+                        </div>
+                        <div class="col">
+                            <div class="row">
+                                <p>DarkUranium</p>
+                            </div>
+                            <div class="row">
+                                <p class="apercuMessage">Vous : D'accord, pas de soucis - 21s</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="card">
+                <div class="card-body">
+                    <div class="row">
+                        <div class="w-25">
+                            <div class="col">
+                                <img class="photoProfile elementDroite" src="{{ asset('images/photo_exemple3.png') }}" alt="default"/>
+                            </div>
+                        </div>
+                        <div class="col">
+                            <div class="row">
+                                <p>EmeraudeDragon</p>
+                            </div>
+                            <div class="row">
+                                <p class="apercuMessage">Vous : Nunc vero inanes flatus quorundam vile es... - 3J</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="card">
+                <div class="card-body">
+                    <div class="row">
+                        <div class="w-25">
+                            <div class="col">
+                                <img class="photoProfile elementDroite" src="{{ asset('images/photo_exemple4.png') }}" alt="default"/>
+                            </div>
+                        </div>
+                        <div class="col">
+                            <div class="row">
+                                <p>Mr.X</p>
+                            </div>
+                            <div class="row">
+                                <p class="apercuMessage">Vous : Nunc vero inanes flatus quorundam vile... - 15min</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="card">
+                <div class="card-body">
+                    <div class="row">
+                        <div class="w-25">
+                            <div class="col">
+                                <img class="photoProfile elementDroite" src="{{ asset('images/photo_exemple5.png') }}" alt="default"/>
+                            </div>
+                        </div>
+                        <div class="col">
+                            <div class="row">
+                                <p>CrabyThunder</p>
+                            </div>
+                            <div class="row">
+                                <p class="apercuMessage">Vous : Nunc vero inanes flatus quorundam vile es... - 3S</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="card">
+                <div class="card-body">
+                    <div class="row">
+                        <div class="w-25">
+                            <div class="col">
+                                <img class="photoProfile elementDroite" src="{{ asset('images/photo_exemple6.png') }}" alt="default"/>
+                            </div>
+                        </div>
+                        <div class="col">
+                            <div class="row">
+                                <p>OscarBrave</p>
+                            </div>
+                            <div class="row">
+                                <p class="apercuMessage">Vous : Nunc vero inanes flatus quorundam vile es... - 23H</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="card">
+                <div class="card-body">
+                    <div class="row">
+                        <div class="w-25">
+                            <div class="col">
+                                <img class="photoProfile elementDroite" src="{{ asset('images/photo_exemple7.png') }}" alt="default"/>
+                            </div>
+                        </div>
+                        <div class="col">
+                            <div class="row">
+                                <p>BearX</p>
+                            </div>
+                            <div class="row">
+                                <p class="apercuMessage">Vous : Nunc vero inanes flatus quorundam vile es... - 23H</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="card">
+                <div class="card-body">
+                    <div class="row">
+                        <div class="w-25">
+                            <div class="col">
+                                <img class="photoProfile elementDroite" src="{{ asset('images/photo_exemple8.png') }}" alt="default"/>
+                            </div>
+                        </div>
+                        <div class="col">
+                            <div class="row">
+                                <p>GhostMaj</p>
+                            </div>
+                            <div class="row">
+                                <p class="apercuMessage">Vous : Nunc vero inanes flatus quorundam vile es... - 23H</p>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
 
-        <div class="card">
-            <div class="card-body">
-                <div class="row">
-                    <div class="w-25">
-                        <div class="col">
-                            <img class="photoProfile elementDroite" src="{{ asset('images/photo_exemple1.png') }}" alt="default"/>
-                        </div>
-                    </div>
-                    <div class="col">
-                        <div class="row">
-                            <p>FrenchBrave</p>
-                        </div>
-                        <div class="row">
-                            <p class="apercuMessage">Cool, heureux d'apprendre que tu vas bien - 2H</p>
-                        </div>
+        <div id="discussion">
+            <div class="bulleD">
+                <div class="imageB">
+                    <img class="photoProfile elementDroite" src="{{ asset('images/photo_default.jpg') }}" alt="default"/>
+                </div>
+                <div class="commentaireB">
+                    <div class="card carteB">
+                        <p>Salut !</p>
                     </div>
                 </div>
             </div>
-        </div>
 
-        <div class="card">
-            <div class="card-body">
-                <div class="row">
-                    <div class="w-25">
-                        <div class="col">
-                            <img class="photoProfile elementDroite" src="{{ asset('images/photo_exemple2.png') }}" alt="default"/>
-                        </div>
+            <div class="bulleR">
+                <div class="commentaireB">
+                    <div class="card carteB">
+                        <p>Salut !</p>
                     </div>
-                    <div class="col">
-                        <div class="row">
-                            <p>DarkUranium</p>
-                        </div>
-                        <div class="row">
-                            <p class="apercuMessage">Vous : D'accord, pas de soucis - 21s</p>
-                        </div>
+                </div>
+                <div class="imageB">
+                    <img class="photoProfile elementDroite" src="{{ asset('images/photo_default.jpg') }}" alt="default"/>
+                </div>
+            </div>
+
+            <div class="bulleR">
+                <div class="commentaireB">
+                    <div class="card carteB">
+                        <p>Salut !</p>
+                    </div>
+                </div>
+                <div class="imageB">
+                    <img class="photoProfile elementDroite" src="{{ asset('images/photo_exemple1.png') }}" alt="default"/>
+                </div>
+            </div>
+
+            <div class="bulleD">
+                <div class="imageB">
+                    <img class="photoProfile elementDroite" src="{{ asset('images/photo_exemple2.png') }}" alt="default"/>
+                </div>
+                <div class="commentaireB">
+                    <div class="card carteB">
+                        <p>Dum haec in oriente aguntur, Arelate hiemem agens Constantius post theatralis ludos atque circenses ambitioso editos apparatu diem sextum idus Octobres, qui imperii eius annum tricensimum terminabat, insolentiae pondera gravius librans, siquid dubium deferebatur aut falsum, pro liquido accipiens et conperto, inter alia excarnificatum Gerontium Magnentianae comitem partis exulari maerore multavit.</p>
                     </div>
                 </div>
             </div>
-        </div>
 
-        <div class="card">
-            <div class="card-body">
-                <div class="row">
-                    <div class="w-25">
-                        <div class="col">
-                            <img class="photoProfile elementDroite" src="{{ asset('images/photo_exemple3.png') }}" alt="default"/>
-                        </div>
-                    </div>
-                    <div class="col">
-                        <div class="row">
-                            <p>EmeraudeDragon</p>
-                        </div>
-                        <div class="row">
-                            <p class="apercuMessage">Vous : Nunc vero inanes flatus quorundam vile es... - 3J</p>
-                        </div>
+            <div class="bulleD">
+                <div class="imageB">
+                    <img class="photoProfile elementDroite" src="{{ asset('images/photo_exemple3.png') }}" alt="default"/>
+                </div>
+                <div class="commentaireB">
+                    <div class="card carteB">
+                        <p>Photo de mon cheval</p>
+                        <img class="photoPublication" src="{{ asset('images/Mon_cheval.jpg') }}" alt="Mon_cheval"/>
                     </div>
                 </div>
             </div>
-        </div>
 
-        <div class="card">
-            <div class="card-body">
-                <div class="row">
-                    <div class="w-25">
-                        <div class="col">
-                            <img class="photoProfile elementDroite" src="{{ asset('images/photo_exemple4.png') }}" alt="default"/>
-                        </div>
-                    </div>
-                    <div class="col">
-                        <div class="row">
-                            <p>Mr.X</p>
-                        </div>
-                        <div class="row">
-                            <p class="apercuMessage">Vous : Nunc vero inanes flatus quorundam vile... - 15min</p>
-                        </div>
+            <div class="bulleR">
+                <div class="commentaireB">
+                    <div class="card carteB">
+                        <p>J'ai trouvé un bug sur ce jeu ce matin.</p>
+                        <video width="500" heigth="500" controls>
+                            <source src="{{ asset('videos/bug.mp4') }}">
+                        </video>
                     </div>
                 </div>
-            </div>
-        </div>
-
-        <div class="card">
-            <div class="card-body">
-                <div class="row">
-                    <div class="w-25">
-                        <div class="col">
-                            <img class="photoProfile elementDroite" src="{{ asset('images/photo_exemple5.png') }}" alt="default"/>
-                        </div>
-                    </div>
-                    <div class="col">
-                        <div class="row">
-                            <p>CrabyThunder</p>
-                        </div>
-                        <div class="row">
-                            <p class="apercuMessage">Vous : Nunc vero inanes flatus quorundam vile es... - 3S</p>
-                        </div>
-                    </div>
+                <div class="imageB">
+                    <img class="photoProfile elementDroite" src="{{ asset('images/photo_exemple4.png') }}" alt="default"/>
                 </div>
             </div>
-        </div>
 
-        <div class="card">
-            <div class="card-body">
-                <div class="row">
-                    <div class="w-25">
-                        <div class="col">
-                            <img class="photoProfile elementDroite" src="{{ asset('images/photo_exemple6.png') }}" alt="default"/>
-                        </div>
-                    </div>
-                    <div class="col">
-                        <div class="row">
-                            <p>OscarBrave</p>
-                        </div>
-                        <div class="row">
-                            <p class="apercuMessage">Vous : Nunc vero inanes flatus quorundam vile es... - 23H</p>
-                        </div>
+            <div class="bulleR">
+                <div class="commentaireB">
+                    <div class="card carteB">
+                        <p>Super musique</p>
+                        <audio controls src="{{ asset('audios/Netrum & Halvorsen - Phoenix [NCS Release].mp3') }}">
                     </div>
                 </div>
-            </div>
-        </div>
-
-        <div class="card">
-            <div class="card-body">
-                <div class="row">
-                    <div class="w-25">
-                        <div class="col">
-                            <img class="photoProfile elementDroite" src="{{ asset('images/photo_exemple7.png') }}" alt="default"/>
-                        </div>
-                    </div>
-                    <div class="col">
-                        <div class="row">
-                            <p>BearX</p>
-                        </div>
-                        <div class="row">
-                            <p class="apercuMessage">Vous : Nunc vero inanes flatus quorundam vile es... - 23H</p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-
-        <div class="card">
-            <div class="card-body">
-                <div class="row">
-                    <div class="w-25">
-                        <div class="col">
-                            <img class="photoProfile elementDroite" src="{{ asset('images/photo_exemple8.png') }}" alt="default"/>
-                        </div>
-                    </div>
-                    <div class="col">
-                        <div class="row">
-                            <p>GhostMaj</p>
-                        </div>
-                        <div class="row">
-                            <p class="apercuMessage">Vous : Nunc vero inanes flatus quorundam vile es... - 23H</p>
-                        </div>
-                    </div>
+                <div class="imageB">
+                    <img class="photoProfile elementDroite" src="{{ asset('images/photo_exemple5.png') }}" alt="default"/>
                 </div>
             </div>
         </div>
